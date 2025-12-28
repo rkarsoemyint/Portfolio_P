1c47eb131b9c96aa86f83c3ed4313702a676fd56
Thant Zin Oo <tzoo2024@gmail.com>
2025-12-28 12:58:47 +0630

    models.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-# ၁။ Project များကို သိမ်းဆည်းမည့် Model
 class Project(models.Model):
-    # Category ရွေးချယ်စရာများ
     CATEGORY_CHOICES = [
         ('web', 'Web Design'),
         ('php', 'PHP Projects'),
@@ -12,7 +10,7 @@ class Project(models.Model):
 
     title = models.CharField(max_length=200, verbose_name="ပရောဂျက်အမည်")
     description = models.TextField(verbose_name="အသေးစိတ်ဖော်ပြချက်")
-    technology = models.CharField(max_length=200, verbose_name="အသုံးပြုထားသော နည်းပညာများ") # e.g. Django, React, MySQL
+    technology = models.CharField(max_length=200, verbose_name="အသုံးပြုထားသော နည်းပညာများ")
     image = models.ImageField(upload_to='project_images/', verbose_name="ပရောဂျက်ပုံ")
     category = models.CharField(
         max_length=20, 
@@ -24,15 +22,13 @@ class Project(models.Model):
     vercel_link = models.URLField(max_length=500, blank=True, null=True, verbose_name="Vercel လင့်ခ်")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="ဖန်တီးသည့်ရက်စွဲ")
 
-    # Admin Panel မှာ နာမည်အတိုင်း ပေါ်နေစေရန်
     def __str__(self):
         return self.title
 
     class Meta:
-        ordering = ['-created_at'] # အသစ်ဆုံးတင်တဲ့ project ကို အပေါ်ဆုံးကပြမယ်
+        ordering = ['-created_at']
 
 
-# ၂။ Contact Form မှ Message များကို သိမ်းဆည်းမည့် Model
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100, verbose_name="အမည်")
     email = models.EmailField(verbose_name="အီးမေးလ်")
