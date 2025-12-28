9a052d7260d8b17c37d4c6959afe183fe622b52c
Thant Zin Oo <tzoo2024@gmail.com>
2025-12-28 12:57:02 +0630

    urls.py

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -6,10 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # ကိုယ့်ရဲ့ app က urls တွေကို ဒီမှာ include လုပ်ပေးရပါမယ်
     path('', include('app.urls')), 
 ]
 
-# Development မှာ ပုံ (Images) တွေ ပေါ်လာအောင် လုပ်ဆောင်ခြင်း
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
